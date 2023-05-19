05977b8d8fc0e1fee822113f58e3584cb264fc34
sclin <read0651@hotmail.com>
2023-05-19 09:20:13 +0800

    Chg: In C#, added some codes about call cpp function.

--- src/TestNetStandardCallNativeCpp/Program.cs
+++ src/TestNetStandardCallNativeCpp/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
 
 class CallNativeCppClass
 {
     public CallNativeCppClass()
-    { }
+    { 
+    }
+
+    [DllImport("NativeCppDll.dll")]
+    static extern void CallNativeCppClass_new();
 }
