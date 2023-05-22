c87dc14dfaca932c5abe201a3ad1528caad825fd
sclin <read0651@hotmail.com>
2023-05-22 13:59:20 +0800

    Chg: Finished .NET and native c++ passing data with common type, e.g. int, double and float.

--- src/NativeCppDll/ImageProcessingFunc.h
+++ src/NativeCppDll/ImageProcessingFunc.h
@@ -4,14 +4,14 @@
 #include <string>
 
 #ifdef DLL_EXPORT
-#define DLL_API extern "C" _declspec(dllexport)
+#define DLL_API extern "C" _declspec(dllexport) 
 #else
-#define DLL_API _declspec(dllimport)
+#define DLL_API _declspec(dllimport) 
 #endif // DLL_EXPORT
 
 extern "C"
 {
-	DLL_API void UsingCommonType(int arg1, double arg2, float arg3, int* output1, double* output2, float* output3);
+	DLL_API void __cdecl UsingCommonType(int arg1, double arg2, float arg3, int* output1, double* output2, float* output3);
 
 	typedef struct _image
 	{
@@ -19,7 +19,7 @@ extern "C"
 		int Height = 0;
 		unsigned char* Data = nullptr;
 	}Image;
-	DLL_API void UsingStruct(Image* inputImage, Image* outputImage);
+	DLL_API void __cdecl UsingStruct(Image* inputImage, Image* outputImage);
 
-	DLL_API void ReleaseImage(Image* inputImage);
+	DLL_API void __cdecl ReleaseImage(Image* inputImage);
 }
--- src/TestNetStandardCallNativeCpp/Program.cs
+++ src/TestNetStandardCallNativeCpp/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Beging testing .NET Standard using native c++ dll.");
+var test = new TestCallNativeCppClass();
+test.TestUsingCommonTypeOutput();
+Console.ReadLine();
 
 class CallNativeCppClass
 {
@@ -10,6 +14,36 @@ class CallNativeCppClass
     { 
     }
 
-    [DllImport("NativeCppDll.dll")]
-    static extern void CallNativeCppClass_new();
+    [DllImport("../../NativeCppDll.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void UsingCommonType(int arg1, double arg2, float arg3, out int output1, out double output2, out float output3);
+}
+
+class TestCallNativeCppClass
+{
+    public TestCallNativeCppClass()
+    { 
+    }
+
+    public void TestUsingCommonTypeOutput()
+    {
+        Console.WriteLine("Test ussing common type and receive output!");
+        int arg1 = 1;
+        double arg2 = 2.0;
+        float arg3 = 3;
+        int output1 = 0;
+        double output2 = 0.0;
+        float output3 = 0;
+
+        CallNativeCppClass.UsingCommonType(arg1, arg2, arg3, out output1, out output2, out output3);
+
+        Debug.Assert(output1 == 2);
+        Debug.Assert(output2 == 4.0);
+        Debug.Assert(output3 == 6);
+
+        Console.WriteLine($"output1 = {output1}, output2 = {output2}, output3 = {output3}");
+    }
+
+    public void TestPassSeftDefineStruct()
+    { 
+    }
 }
