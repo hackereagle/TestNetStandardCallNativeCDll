cc24683ee2cf1fbceb5b255e456f5ddfdb555b72
sclin <read0651@hotmail.com>
2023-06-28 16:34:34 +0800

    Moved convert Bitmap to ImageStruct method become ImageStruct constructor.

--- src/TestNetStandardCallNativeCpp/Program.cs
+++ src/TestNetStandardCallNativeCpp/Program.cs
@@ -54,42 +54,39 @@ class TestCallNativeCppClass
         public int Width;
         public int Height;
         public IntPtr Data;
-    }
 
-    ImageStruct ConvertBitmap2ImageStruct(Bitmap bm)
-    { 
-        ImageStruct ret = new ImageStruct();
-        ret.Width = bm.Width;
-        ret.Height = bm.Height;
-        byte[] Date = new byte[bm.Width * bm.Height];
+        public ImageStruct(Bitmap bm)
+        { 
+            this.Width = bm.Width;
+            this.Height = bm.Height;
+            byte[] Date = new byte[bm.Width * bm.Height];
 
-        BitmapData bd = bm.LockBits(new Rectangle(0, 0, bm.Width, bm.Height), ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
-        int stride = bd.Stride;
-        int skipByte = stride - bd.Width;
-        int size = bm.Width * bm.Height;
-        unsafe
-        {
-            byte* p = (byte*)bd.Scan0.ToPointer();
-            int colIdx = 0;
-            for (int i = 0; i < size; i++)
+            BitmapData bd = bm.LockBits(new Rectangle(0, 0, bm.Width, bm.Height), ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
+            int stride = bd.Stride;
+            int skipByte = stride - bd.Width;
+            int size = bm.Width * bm.Height;
+            unsafe
             {
-                Date[i] = *p;
-                p = p + 1;
-                colIdx = colIdx + 1;
-                if (colIdx == bm.Width - 1)
-                { 
-                    p = p + skipByte;
-                    colIdx = 0;
+                byte* p = (byte*)bd.Scan0.ToPointer();
+                int colIdx = 0;
+                for (int i = 0; i < size; i++)
+                {
+                    Date[i] = *p;
+                    p = p + 1;
+                    colIdx = colIdx + 1;
+                    if (colIdx == bm.Width - 1)
+                    { 
+                        p = p + skipByte;
+                        colIdx = 0;
+                    }
                 }
             }
+            bm.UnlockBits( bd );
+
+            GCHandle pinnedArray = GCHandle.Alloc( Date, GCHandleType.Pinned);
+            this.Data = pinnedArray.AddrOfPinnedObject();
+            pinnedArray.Free();
         }
-        bm.UnlockBits( bd );
-
-        GCHandle pinnedArray = GCHandle.Alloc( Date, GCHandleType.Pinned);
-        ret.Data = pinnedArray.AddrOfPinnedObject();
-        pinnedArray.Free();
-
-        return ret;
     }
 
     [DllImport("../../NativeCppDll.dll", EntryPoint = "UsingStruct", CallingConvention = CallingConvention.Cdecl)]
@@ -100,7 +97,7 @@ class TestCallNativeCppClass
         Console.WriteLine($"\n\n***** Test pass seft definition struct!");
         System.Drawing.Bitmap img = new System.Drawing.Bitmap(AppDomain.CurrentDomain.BaseDirectory + "../../../Images/lena_gray.bmp");
         Console.WriteLine($"Read image: width = {img.Width}, height = {img.Height}, pixel format = {img.PixelFormat.ToString()}");
-        ImageStruct input = ConvertBitmap2ImageStruct(img);
+        ImageStruct input = new ImageStruct(img);
         ImageStruct output = new ImageStruct();
 
         UsingStruct(input, ref output, 1, 2.0);
